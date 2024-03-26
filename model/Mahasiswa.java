b652ebb51702887436c29bb9bdbfc194a4b1c4ee
NajmyW <najmywardhana16@gmail.com>
2024-03-26 09:57:49 +0700

    Memanggil Method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Mahasiswa {
+    public static void tampilanInfo(){
+        System.out.println();
+        System.out.println("== Data Mahasiswa ====================");
+        System.out.println("NPM : 2210010212");
+        System.out.println("Nama: Muhammad Najmy Wardhana");
+        System.out.println("=======================================");}
+}
