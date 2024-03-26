bd3dcb89d4e87fd71d08926359d5fa18932475e6
NajmyW <najmywardhana16@gmail.com>
2024-03-26 10:20:38 +0700

    Modifikasi File Input Output

--- InputOutput.java
+++ InputOutput.java
@@ -4,9 +4,17 @@ public class InputOutput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String nama;
+        int jumlahSks;
+        double IPK;
         System.out.print("Masukkan nama anda: ");
         nama = scanner.nextLine();
         System.out.println("Hai, "+nama);
+        System.out.print("Masukkan Jumlah SKS yang sudah ditempuh: ");
+        jumlahSks = scanner.nextInt();
+        System.out.print("Masukan IPK Sementara: ");
+        IPK = scanner.nextDouble();
+        double jumlah = IPK * jumlahSks;
+        System.out.println("Total Nilai: "+jumlah);
         scanner.close();
     }
 }
